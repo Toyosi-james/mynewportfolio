f07d4682418bf35dc55e091f5de65c559b23c935
Toyosi-james <Jamestoyosi84@gmail.com>
2026-06-22 17:01:23 +0100

    removed movie app and changed cv

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
                </ul>
        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4 '>
         <a href='#contact'  className='bg-black text-white px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>contact me  <Image src={assets.right_arrow_white} alt='' className='rounded-full w-4'/></a>
-        <a href='/James Oluwatoyosi Gift Resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my resume <Image src={assets.download_icon} alt='' className='rounded-full w-4'/></a>
+        <a href='/TOYOSI CV 3.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my resume <Image src={assets.download_icon} alt='' className='rounded-full w-4'/></a>
        </div>
     </div>
   )
--- src/app/components/Work.jsx
+++ src/app/components/Work.jsx
@@ -25,7 +25,8 @@ const Work = () => {
         </div>
 
         {/* Movie App */}
-        <div className="w-[350px] bg-[#fcf4ff] dark:bg-gray-800 pb-4 hover:scale-[1.02] border border-gray-400 dark:border-gray-600 rounded-lg
+
+        {/* <div className="w-[350px] bg-[#fcf4ff] dark:bg-gray-800 pb-4 hover:scale-[1.02] border border-gray-400 dark:border-gray-600 rounded-lg
                         hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] dark:hover:bg-gray-700 hover:-translate-y-1 duration-500">
           <img className="w-[350px] mb-5 rounded-t-lg" src="/movie.png" alt="" />
           <div className="px-3">
@@ -36,19 +37,19 @@ const Work = () => {
             <a className="bg-black text-[#fcf4ff] p-2 font-bold rounded-[5px]" href="https://github.com/Toyosi-james/movieBox" target="_blank" rel="noopener noreferrer">View Project</a>
             <a className="border-2 border-gray-400 dark:border-gray-600 p-2 rounded-[5px] font-bold" href="https://toyosi-james.github.io/movieBox/" target="_blank" rel="noopener noreferrer">Live Demo</a>
           </div>
-        </div>
+        </div> */}
 
         {/* Moove Homepage */}
         <div className="w-[350px] bg-[#fcf4ff] dark:bg-gray-800 pb-4 hover:scale-[1.02] border border-gray-400 dark:border-gray-600 rounded-lg
                         hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] dark:hover:bg-gray-700 hover:-translate-y-1 duration-500">
-          <img className="w-[350px] mb-5 rounded-t-lg" src="/moove.png" alt="" />
+          <img className="w-[350px] mb-5 rounded-t-lg" src="/crypto.png" alt="" />
           <div className="px-3">
-            <h1 className="text-[18px] font-bold text-black dark:text-white">Moove Homepage Clone</h1>
-            <p className="text-gray-700 dark:text-gray-300 font-bold">Tech Stack: HTML, CSS</p>
+            <h1 className="text-[18px] font-bold text-black dark:text-white">CryptoPulse App</h1>
+            <p className="text-gray-700 dark:text-gray-300 font-bold">Tech Stack: Next Js, Tailwind Css, API</p>
           </div>
           <div className="px-3 py-4 flex items-center gap-3">
-            <a className="bg-black text-[#fcf4ff] p-2 font-bold rounded-[5px]" href="https://github.com/Toyosi-james/Toyosi-james-moove-responsive-website." target="_blank" rel="noopener noreferrer">View Project</a>
-            <a className="border-2 border-gray-400 dark:border-gray-600 p-2 rounded-[5px] font-bold" href="https://toyosi-james.github.io/Toyosi-james-moove-responsive-website./" target="_blank" rel="noopener noreferrer">Live Demo</a>
+            <a className="bg-black text-[#fcf4ff] p-2 font-bold rounded-[5px]" href="https://github.com/Toyosi-james/cryptoPulse" target="_blank" rel="noopener noreferrer">View Project</a>
+            <a className="border-2 border-gray-400 dark:border-gray-600 p-2 rounded-[5px] font-bold" href="https://crypto-pulse-murex.vercel.app/" target="_blank" rel="noopener noreferrer">Live Demo</a>
           </div>
         </div>
 
